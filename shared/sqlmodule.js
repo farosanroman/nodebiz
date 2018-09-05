b5dfa72ba7de8c1cbbe0435ebdbfd25b1e605164
farosanroman <ppazpurua@gmail.com>
2018-09-05 19:14:46 -0400

    actua

--- shared/sqlmodule.js
+++ shared/sqlmodule.js
@@ -5,7 +5,10 @@ var sqlmodule = {
         const sql = require("mssql");
         // Configuration object for your database
         const config = {
-            
+            user: 'admin',
+            password: 'pag9539961$',
+            server: '206.72.117.220', 
+            database: 'fuentes2015' 
         };
         // connect to the database
         
@@ -16,7 +19,7 @@ var sqlmodule = {
            // query to the database and get the records
            const qry="MM_CONT_BAL 'INAZZ20',201808,201808 ,0,0,'1442',11,1,4,0,0,0,'0' ";
             request.query(qry, function (err, recordset) {
-                //console.log(recordset)
+                console.log(recordset)
               if (err) {
                    callback(error);
                    sql.close()
