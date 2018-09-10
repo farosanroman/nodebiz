7faf817cb4f4145aec47664d795f6b9f71ea9ab9
farosanroman <ppazpurua@gmail.com>
2018-09-10 17:59:07 -0400

    ppa

--- app.js
+++ app.js
@@ -17,26 +17,45 @@ app.get('/',(req,res)=>{
     console.log(req.query)
     const plan=req.query.plan
     const ta=req.query.ta
-    var qry,criteria,where;
+    var qry,criteria;
+    var where="";
     if (plan=="ta"){
       qry=queries.ta;  
       where=  "where Documento='TIPOAUXILIAR'"    
     }
     if (plan=="cuenta"){
       qry=queries.cuentas;  
-      where=  "where auxiliar='"+ta+"'"    
+      if (ta!=""){
+        console.log("ta"+ta+"ta")
+         where=  "where auxiliar='"+ta+"' and nivel=4" 
+      }else{
+        where=  "where nivel=4"
+      }   
     }
     if (plan=="auxi"){
       qry=queries.auxiliares;
-      where=  "where tipo='"+ta+"'"    
+      if (ta!=""){
+          where=  "where tipo='"+ta+"'"   
+      } 
     }
-    
+
       qry=qry.replace(/#where#/i, where);
+    
       criteria={qry:qry}
        console.log(criteria)
       SQL.sqlrequest(criteria, function (err, results) { 
          res.send(results);
       })
+  });
+  app.get('/cont/comprobante',(req,res)=>{
+    qry=queries.comprobante;
+    var where="where A.Num_Comprob = 'CIE201712999999' and A.Periodo = 201712 and A.Cia = '0000000'";
+    qry=qry.replace(/#where#/i, where);
+    const criteria={qry:qry}
+    //console.log(criteria)
+    SQL.sqlrequest(criteria, function (err, results) { 
+         res.send(results);
+    })
   });
   app.get('/cont/insert',(req,res)=>{
     const criteria={qry:queries.insert}
@@ -46,28 +65,21 @@ app.get('/',(req,res)=>{
       })
   });
   
-  app.get('/cont/comprobante',(req,res)=>{
-    const criteria={qry:queries.comprobante }
-    //console.log(criteria)
-    SQL.sqlexecute(criteria, function (err, results) { 
-         res.send(results);
-    })
-  });
+
   app.get('/cont/balgen',(req,res)=>{
-      //const row=comprobante.find(c=>c.id===parseInt(req.params.id));
+      
+  const criteria={qry:queries.balgen }
+    SQL.sqlrequest(criteria, function (err, results) { 
+     // console.log(results) 
+         res.send(results);
+     })
+     //const row=comprobante.find(c=>c.id===parseInt(req.params.id));
       //if (!row) res.status(404).send('malo malo');
 
     //res.send(row);
     //const criteria={id:"ppa"}
-  const criteria={qry:queries.balgen }
-    SQL.sqlexecute(criteria, function (err, results) { 
-     // console.log(results) 
-         res.send(results);
-     })
   });
-  //app.listen(process.env.PORT || 3000); 
-
-  //  app.listen(process.env.PORT || 4730); 
+  
 const port=process.env.PORT || 4730
 app.listen(port,()=>console.log(`Listening on Port ${port}...`));
   
