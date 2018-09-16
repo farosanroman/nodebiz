1166cf1148cfcbe68d69fb0df82249015890becf
Faro San Roman <ppazpurua@gmail.com>
2018-09-16 13:23:21 -0400

    Update sqlmodule.js

--- shared/sqlmodule.js
+++ shared/sqlmodule.js
@@ -1,8 +1,8 @@
 const config = {
     user: 'admin',
-    password: 'pag9539961$',
-    server: '206.72.117.220', 
-    database: 'fuentes2015'
+    password: '$$$$',
+    server: '206.200.200.200', 
+    database: 'pwwwwwwd'
 };
 var sqlmodule = {
     
