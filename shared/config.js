bc204a585f98f28fbaf028c143298696416f30e9
farosanroman <ppazpurua@gmail.com>
2018-09-24 21:08:10 -0400

    ppa

--- shared/config.js
+++ shared/config.js
@@ -1,13 +1,5 @@
 const config = {
-    user: 'admin',
-    password: 'pag9539961$',
-    server: '206.72.117.220', 
-    database: 'bizsecurity',
-    
-    usercia: 'admin',
-    passwordcia: 'pag9539961$',
-    servercia: '206.72.117.220', 
-    databasecia: 'fuentes2015'
+
 };
 module.exports = config;
 
